5397c5c6f71da7c4da0d7bc299ac13d34d2a3ae6
Juan Alfredo Corona Bejarano <alfredocbejarano@gmail.com>
2016-05-20 13:12:10 -0500

    Adds found dog flow page

--- js/dog_wrapper.js
+++ js/dog_wrapper.js
@@ -94,3 +94,56 @@ function show(dogID) {
 		}			
 	);
 }
+
+function progressBar(step) {
+	var bar = document.getElementById('bar');
+	var number = 14;
+	var ending = "%;"
+	var style = "width:"
+	var text = "% Completado"
+	var button =  document.getElementById('submit');
+
+	var forms = document.getElementsByClassName('form');
+
+	/* FORM elements */
+	var breed = document.getElementById('breed');
+	var color = document.getElementById('color');
+	/* Step 2 */
+	var gender =  document.getElementsByName('gender');
+
+	forms[step-1].style.display = "none";
+	forms[step-1].style.opacity = 0;
+
+	forms[step].style.display = "block";
+	forms[step].style.opacity = 1;
+
+	if(step == 1 ) {
+		bar.innerHTML = number+text;
+		bar.setAttribute("style",style+number+ending);
+		button.setAttribute("onclick","progressBar(2);");
+	} else if (step == 2) {
+		bar.innerHTML = (number * 2)+text;
+		bar.setAttribute("style",style+(number * 2)+ending);
+		button.setAttribute("onclick","progressBar(3);");		
+	} else if (step == 3) {
+		bar.innerHTML = (number * 3)+text;
+		bar.setAttribute("style",style+(number * 3)+ending);
+		button.setAttribute("onclick","progressBar(4);");
+	} else if (step == 4) {
+		bar.innerHTML = (number * 4)+text;
+		bar.setAttribute("style",style+(number * 4)+ending);
+		button.setAttribute("onclick","progressBar(5);");
+	}else if (step == 5) {
+		bar.innerHTML = (number * 5)+text;
+		bar.setAttribute("style",style+(number * 5)+ending);
+		button.setAttribute("onclick","progressBar(6);");
+	}else if (step == 6) {
+		bar.innerHTML = (number * 6)+text;
+		bar.setAttribute("style",style+(number * 6)+ending);
+		button.setAttribute("onclick","progressBar(7);");
+	} else {
+		bar.innerHTML = "100"+text;
+		bar.setAttribute("style",style+"100"+ending);
+		button.setAttribute("onclick","progressBar(0);");
+	}
+}
